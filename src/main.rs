b35d4c52de7a5c3dc3b8f6259672af322ed74222
zarkius <diegomallor2.0@gmail.com>
2025-01-01 15:11:51 +0100

    multiplataforma added

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,23 @@
-use std::io;
 use sys_info::{self};
 extern crate glium;
 use glium::glutin::event_loop::EventLoop;
 use glium::glutin::window::WindowBuilder;
 use glium::Surface;
 
-fn imprimir_info_sistema() {
-    let os_version = sys_info::os_release().unwrap_or_else(|_| "Desconocida".to_string());
-    let os_type = sys_info::os_type().unwrap_or_else(|_| "Desconocido".to_string());
-    let cpu_vendor = "Desconocido".to_string();
-    let cpu_frequency = sys_info::cpu_speed().unwrap_or_else(|_| 0);
-    let cpu_num_cores = sys_info::cpu_num().unwrap_or_else(|_| 0);
-    let mem_info = sys_info::mem_info().unwrap_or_else(|_| sys_info::MemInfo {
-        total: 0,
-        free: 0,
-        avail: 0,
-        buffers: 0,
-        cached: 0,
-        swap_total: 0,
-        swap_free: 0,
-    });
-    let disk_info = sys_info::disk_info().unwrap_or_else(|_| sys_info::DiskInfo {
-        total: 0,
-        free: 0,
-    });
-
-    println!("Sistema Operativo: {} {}", os_type, os_version);
-    println!("CPU: {} @ {} MHz, {} núcleos", cpu_vendor, cpu_frequency, cpu_num_cores);
-    println!("Memoria: {} KB total, {} KB libre", mem_info.total, mem_info.free);
-    println!("Disco: {} KB total, {} KB libre", disk_info.total, disk_info.free);
-}
-
-fn main() {
-    imprimir_info_sistema();
-    println!("\n\n\n");
-    let mut nombre_usuario = String::new();
-    println!("Hola, ¿cómo te llamas?");
-    io::stdin().read_line(&mut nombre_usuario).expect("Error al leer la entrada");
-    println!("Hola, {} , tu aventura te aguarda!", nombre_usuario.trim());
-    
+fn generar_ventana_linux() {
     let event_loop = EventLoop::new();
     let window_builder = WindowBuilder::new()
-        .with_title("Ventana Nativa con Glium y Winit")
+        .with_title("Reemplaza por tu título")
         .with_inner_size(glium::glutin::dpi::LogicalSize::new(800.0, 600.0))
-        .with_min_inner_size(glium::glutin::dpi::LogicalSize::new(400.0, 300.0)); // Tamaño mínimo de la ventana
-
+        .with_min_inner_size(glium::glutin::dpi::LogicalSize::new(400.0, 300.0));
+    // Tamaño mínimo de la ventana
+    
     let context_builder = glium::glutin::ContextBuilder::new();
     let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
-
+    
     event_loop.run(move |event, _, control_flow| {
         *control_flow = glium::glutin::event_loop::ControlFlow::Wait;
-
+    
         match event {
             glium::glutin::event::Event::WindowEvent { event, .. } => match event {
                 glium::glutin::event::WindowEvent::CloseRequested => {
@@ -60,9 +27,62 @@ fn main() {
             },
             _ => (),
         }
-
+    
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
         target.finish().unwrap();
     });
+    }
+
+
+
+    fn imprimir_info_sistema() {
+        let os_type = sys_info::os_type().unwrap_or_else(|_| "Desconocido".to_string());
+        let mem_info = format!("{} KB total, {} KB libre", sys_info::mem_info().unwrap_or_else(|_| sys_info::MemInfo {
+            total: 0,
+            free: 0,
+            avail: 0,
+            buffers: 0,
+            cached: 0,
+            swap_total: 0,
+            swap_free: 0,
+        }).total, sys_info::mem_info().unwrap_or_else(|_| sys_info::MemInfo {
+            total: 0,
+            free: 0,
+            avail: 0,
+            buffers: 0,
+            cached: 0,
+            swap_total: 0,
+            swap_free: 0,
+        }).free);
+        let disk_info = format!("{} KB total, {} KB libre", sys_info::disk_info().unwrap_or_else(|_| sys_info::DiskInfo {
+            total: 0,
+            free: 0,
+        }).total, sys_info::disk_info().unwrap_or_else(|_| sys_info::DiskInfo {
+            total: 0,
+            free: 0,
+        }).free);
+
+
+        let array_strings: [String; 3] = [os_type, mem_info, disk_info];
+
+        println!("Información del sistema: {} \n {} \n {} \n", array_strings[0], array_strings[1], array_strings[2]);
+    }
+
+#[cfg(target_os = "windows")]
+fn main() {
+    imprimir_info_sistema();
+    generar_ventana_windows();
+}
+
+#[cfg(target_os = "linux")]
+fn main() {
+    imprimir_info_sistema();
+    generar_ventana_linux();
+}
+
+#[cfg(target_os = "macos")]
+fn main() {
+    imprimir_info_sistema();
+    generar_ventana_macos();
 }
