2437865b2deec6d188a3ca8ec1b52b46825be54f
zarkius <diegomallor2.0@gmail.com>
2025-01-14 13:59:34 +0100

    SOPORTE MAC WINDOWS Y LINUX

---
+++ target/debug/deps/PokeMonster-11c4c47a9a620b0a.d
@@ -0,0 +1,5 @@
+/home/v1/Escritorio/proyectos Rust/Rust/target/debug/deps/libPokeMonster-11c4c47a9a620b0a.rmeta: src/main.rs
+
+/home/v1/Escritorio/proyectos Rust/Rust/target/debug/deps/PokeMonster-11c4c47a9a620b0a.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/PokeMonster-be90ed070d099e12.d
@@ -0,0 +1,5 @@
+/home/v1/Escritorio/proyectos Rust/Rust/target/debug/deps/libPokeMonster-be90ed070d099e12.rmeta: src/main.rs
+
+/home/v1/Escritorio/proyectos Rust/Rust/target/debug/deps/PokeMonster-be90ed070d099e12.d: src/main.rs
+
+src/main.rs:
